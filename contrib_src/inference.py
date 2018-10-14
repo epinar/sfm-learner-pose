5a446e298334dc01008c63a7f834a49dcc2709d2
epinar <evin.oernek@tum.de>
2018-10-14 21:38:44 +0200

    pose graph added

--- contrib_src/inference.py
+++ contrib_src/inference.py
@@ -1,9 +1,12 @@
-import onnx
-import caffe2.python.onnx.backend
+#import onnx
+import tensorflow as tf
 import json
 from processing import ImageProcessor
 from modelhublib.model import ModelBase
-
+import PIL
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.saved_model import tag_constants
 
 class Model(ModelBase):
 
@@ -13,16 +16,28 @@ class Model(ModelBase):
         # get the image processor
         self._imageProcessor = ImageProcessor(config)
         # load the DL model (change this if you are not using ONNX)
-        self._model = onnx.load('model/model.onnx')
+        #self._model = onnx.load('model/model.onnx')
+        #self._model = sfm
     
 
     def infer(self, input):
-        # load preprocessed input
         inputAsNpArr = self._imageProcessor.loadAndPreprocess(input)
-        # Run inference with caffe2 (change this if you are using a different DL framework)
-        results = caffe2.python.onnx.backend.run_model(self._model, [inputAsNpArr])
-        # postprocess results into output
-        output = self._imageProcessor.computeOutput(results)
+        # load preprocessed input
+        graph = tf.Graph()
+        with graph.as_default():
+            with tf.Session(graph=graph) as sess:
+                tf.saved_model.loader.load(
+                    sess,
+                    [tag_constants.SERVING],
+                    'model/',
+                )
+                input_uint8 = graph.get_tensor_by_name('raw_input:0')
+                prediction = graph.get_tensor_by_name('pose_prediction/pose_exp_net/pose/mul:0')
+
+                pred = sess.run(prediction, feed_dict={input_uint8: inputAsNpArr[None,:,:,:]})
+
+        output = self._imageProcessor.computeOutput(pred)
         return output
+
         
 
